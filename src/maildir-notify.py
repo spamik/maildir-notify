47b5d9d3c055bcf2eaf0a6de65b8deecc1356100
Jan Krajdl <spm@spamik.cz>
2010-10-16 23:41:50 +0200

    Indent change
    Project imported to anjuta, indent change for parsing with anjuta plugin.

--- src/maildir-notify.py
+++ src/maildir-notify.py
@@ -10,47 +10,47 @@ from email.parser import Parser
 from email.header import decode_header
 
 def loadFolders(folders):
-    # parse specified maildir folders and sort them
-    res = []
-    p = re.compile("dir_(?P<num>[\d]+)")
-    for i, j in folders:
-        m = p.match(i)
-        if(m):
-            res.append((m.group('num'), os.path.expanduser(j) + '/new'))
-    res.sort(key = lambda x: x[0])
-    return res
+	# parse specified maildir folders and sort them
+	res = []
+	p = re.compile("dir_(?P<num>[\d]+)")
+	for i, j in folders:
+		m = p.match(i)
+		if(m):
+			res.append((m.group('num'), os.path.expanduser(j) + '/new'))
+	res.sort(key = lambda x: x[0])
+	return res
 
 def scanNew(folders):
-    # find new messages in folders
-    for i, j in folders:
-        print "Scanning folder", i
-        if(not os.path.isdir(j)):
-            print "Folder num", i, "is not a valid maildir folder."
-            continue
-        dir = os.listdir(j)
-        for k in dir:
-            f = open(j + '/' + k, 'r')
-            msg = f.read()
-            f.close()
-            headers = Parser().parsestr(msg)
-            print "New msg"
-            print "From:", decode_header(headers['from'])[0][0]
-            print "Subject:", decode_header(headers['subject'])[0][0]
-
+	# find new messages in folders
+	for i, j in folders:
+		print "Scanning folder", i
+		if(not os.path.isdir(j)):
+			print "Folder num", i, "is not a valid maildir folder."
+			continue
+		dir = os.listdir(j)
+		for k in dir:
+			f = open(j + '/' + k, 'r')
+			msg = f.read()
+			f.close()
+			headers = Parser().parsestr(msg)
+			print "New msg"
+			print "From:", decode_header(headers['from'])[0][0]
+			print "Subject:", decode_header(headers['subject'])[0][0]
+			
 def main():
-    path = os.path.expanduser("~/.maildir-notify.conf")
-    if(not os.path.isfile(path)):
-        print "Configuration file ~/.maildir-notify.conf does not exists."
-        print "Please create your configuration first."
-        return
-    cfg = ConfigParser.RawConfigParser()
-    cfg.read(path)
-    if(not cfg.has_section('maildir_folders')):
-        print "You haven't specified any maildir folders to watch."
-        print "Please edit your config file first."
-        return
-    folders = loadFolders(cfg.items('maildir_folders'))
-    scanNew(folders)
+	path = os.path.expanduser("~/.maildir-notify.conf")
+	if(not os.path.isfile(path)):
+		print "Configuration file ~/.maildir-notify.conf does not exists."
+		print "Please create your configuration first."
+		return
+	cfg = ConfigParser.RawConfigParser()
+	cfg.read(path)
+	if(not cfg.has_section('maildir_folders')):
+		print "You haven't specified any maildir folders to watch."
+		print "Please edit your config file first."
+		return
+	folders = loadFolders(cfg.items('maildir_folders'))
+	scanNew(folders)
 
 main()
 
